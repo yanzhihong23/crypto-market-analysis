7135f36cbe7b100a22684900751250c5deead9ff
Hom Yan <yanzhihong23@qq.com>
2023-03-03 17:00:54 +0800

    chore: add type enum

--- .commitlintrc.cjs
+++ .commitlintrc.cjs
@@ -2,7 +2,7 @@ module.exports = {
   extends: ["@commitlint/config-conventional"],
   rules: {
     'type-enum': [2, 'always', [
-      'feat', 'fix', 'refactor', 'style', 'chore', 'perf', 'docs'
+      'feat', 'fix', 'refactor', 'style', 'chore', 'perf', 'docs', 'build', 'ci', 'revert', 'test'
     ]]
   }
 }
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,5 +1,4 @@
 function Home() {
-  console.log('test 1')
   return <h1>Home</h1>
 }
 
