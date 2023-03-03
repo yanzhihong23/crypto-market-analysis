8c99d6d0c0a8c9be0708afeff2cc0d46548b53c8
Hom Yan <yanzhihong23@qq.com>
2023-03-03 16:42:05 +0800

    test 1

--- .commitlintrc.cjs
+++ .commitlintrc.cjs
@@ -1,3 +1,8 @@
 module.exports = {
-  extends: ["@commitlint/config-conventional"]
+  extends: ["@commitlint/config-conventional"],
+  rules: {
+    'type-enum': [2, 'always', [
+      'feat', 'fix', 'refactor', 'style', 'chore', 'perf', 'docs'
+    ]]
+  }
 }
