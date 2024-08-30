7ccd621ca4c1ec411c826bab70f96cf59f13372c
Hom Yan <yanzhihong23@qq.com>
2024-08-30 10:40:46 +0800

    fix: remove base config

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,6 @@ import viteEslint from 'vite-plugin-eslint'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/crypto-market-analysis/',
   plugins: [
     react(),
     viteEslint({
