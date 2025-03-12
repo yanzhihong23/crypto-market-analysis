a8ad70e76151bc6b59316bf1d24af1fdce467368
Hom Yan <yanzhihong23@qq.com>
2025-03-12 14:00:52 +0800

    feat: logo

--- src/components/TopBar.tsx
+++ src/components/TopBar.tsx
@@ -1,6 +1,8 @@
 import { AppBar, Stack, Toolbar, Typography } from '@mui/material'
 import { useEffect, useState } from 'react'
 
+import Logo from '../assets/logo.svg?react'
+
 import NavMenu from './NavMenu'
 
 export default function TopBar() {
@@ -35,30 +37,7 @@ export default function TopBar() {
         }}
       >
         <Stack direction="row" alignItems="center" gap={1}>
-          <svg
-            width="32"
-            height="32"
-            viewBox="0 0 32 32"
-            fill="none"
-            xmlns="http://www.w3.org/2000/svg"
-          >
-            <path
-              d="M16 2L4 8V24L16 30L28 24V8L16 2Z"
-              fill="currentColor"
-              fillOpacity="0.2"
-            />
-            <path
-              d="M16 6L8 10V22L16 26L24 22V10L16 6Z"
-              stroke="currentColor"
-              strokeWidth="2"
-            />
-            <path
-              d="M16 12V20M12 16H20"
-              stroke="currentColor"
-              strokeWidth="2"
-              strokeLinecap="round"
-            />
-          </svg>
+          <Logo />
           <Typography variant="h6" fontWeight={700} pr={4}>
             Perpetual Market
           </Typography>
