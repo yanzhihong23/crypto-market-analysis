728d87f6c04df17ec2f6f4ac799b2ea8cf0888ef
Hom Yan <yanzhihong23@qq.com>
2025-06-01 12:35:57 +0800

    fix: proxy

--- src/apis/util.ts
+++ src/apis/util.ts
@@ -1,5 +1,8 @@
 export const proxyGet = async (url: string) => {
   const res = await fetch(`https://verio-api.vercel.app/api/proxy?${url}`)
   const data = await res.json()
+  if (data.code === '0') {
+    return data.data
+  }
   return data
 }
